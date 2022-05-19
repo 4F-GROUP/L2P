a874c6e7d7997587a4adf6c2af67f99915b94f88
Gabriela Rosa <gabrielarosacmp@gmail.com>
2022-05-18 23:55:12 -0300

    correção do css da home

--- home.php
+++ home.php
@@ -16,9 +16,24 @@
         </nav>
         </div>
         <div class="menu">
-            <div class="example-item example-item_1"> <a href="cadastro.php"> <img src="Imagens/cadastro 1.png"></a> Cadastro Estudante  </div>
-            <div class="example-item example-item_3"> <a href="cadastro.funcionario.php"> <img src="Imagens/login 1.png"> </a>Cadastro Funcionário </div>
-            <div class="example-item example-item_2"> <a href="estudantes.php"> <img src="Imagens/listar 1.png"></a> Alunos  </div>
+            <div class="example-item example-item_1"> 
+                <a href="cadastro.php"> 
+                    <img src="Imagens/cadastro2.png" style="width: 90%; height: 90%;">
+                </a> 
+                <p style="text-align: center; margin-top: 8%;">Cadastro Estudante</p> 
+            </div>
+            <div class="example-item example-item_3"> 
+                <a href="cadastro.funcionario.php"> 
+                    <img src="Imagens/cadastro2.png" style="width: 90%; height: 90%;"> 
+                </a>
+                <p style="text-align: center; margin-top: 8%;">Cadastro Funcionário</p> 
+            </div>
+            <div class="example-item example-item_2"> 
+                <a href="estudantes.php"> 
+                    <img src="Imagens/lista-de-compras.png"  style="width: 90%; height: 90%;">
+                </a> 
+                <p style="text-align: center; margin-top: 13%;">Alunos</p>   
+            </div>
         </div>
     </div> 
 </body>
