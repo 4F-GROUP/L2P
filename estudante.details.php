68b1cf0a86cddb9dc72148cc840c1ce9a3873dbb
Thalis Mateus <thalis.alcapone@gmail.com>
2022-05-19 16:56:17 -0300

    Correção

--- estudante.details.php
+++ estudante.details.php
@@ -20,7 +20,35 @@
 <img src="Imagens\user.png" height="20%" width="20%" alt="">
 </div>
 <body>
-
+<form method="POST" enctype="multipart/form-data">		
+  <div class="row g-3 mb-2" style="margin-left: 5%;">
+    <input  style="width: 80%;" type="file" name="pic" accept="image/*" class="form-control">
+    <div class="col-sm" style="margin-left: 55px;">
+      <button type="submit" class="btn btn-warning link-dark">Enviar imagem</button>
+    </div>
+  </div>
+  </form>
+  <?php
+      if(isset($_FILES['pic']))
+      {
+        $ext = strtolower(substr($_FILES['pic']['name'],-4)); //Pegando extensão do arquivo
+        $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
+        $dir = './imagens/'; //Diretório para uploads
+  
+        move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
+          echo '<div class="alert alert-success" role="alert" align="center">
+      
+      <img src="./imagens/' . $new_name . '" class="img img-responsive img-thumbnail" width="200"> 
+      <br>
+      Imagem enviada com sucesso!
+      <br>
+      <a href="exemplo_upload_de_imagens.php">
+      <button class="btn btn-default">Enviar nova imagem</button>
+      </a>
+      
+      </div>';
+      }  
+  ?>
 <section class="page-section" id="estudante">
     <div class="container">
         <div class="text-center">
