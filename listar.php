584fb43c72ddb0208a73d1ab0d14a3b42e4f7ac8
Arkymad <phmm2003@hotmail.com>
2022-05-19 17:21:39 -0300

    aaa

--- listar.php
+++ listar.php
@@ -30,7 +30,7 @@
         <input type="text" class="form-control" placeholder="e-mail" aria-label="E-mail" >
     </div>       
     <br>
-    <button class="btn btn-outline-primary" type="submit">Pesquisar</button>
+    <button class="btn btn-warning link-dark" type="submit">Pesquisar</button>
     </form>
 </body>
 </html>
